370ac5f59488badec8db93851d1520569c58ddff
Lucas <lucas.camargo.silva98@gmail.com>
2018-12-18 21:10:38 -0200

    carousel home

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,22 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, ViewChild, TemplateRef } from '@angular/core';
+import { NavController, Slides } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
+
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+  @ViewChild(Slides)slide: Slides
+  
+  arrSlides = [{img:'../../assets/imgs/upa.png'},{img:'../../assets/imgs/ump.png'},{img:'../../assets/imgs/uph.png'},{img:'../../assets/imgs/saf.png'}]
+  constructor(){
+    
   }
+
+ ionViewDidLoad(){
+  this.slide.autoplayDisableOnInteraction = false
+ }
 
 }
